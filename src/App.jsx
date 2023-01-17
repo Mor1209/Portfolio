0ea3669df740b1a0908ba4056dc03bf4e33ffd8b
moritzHauptmann <mohauptmann@gmail.com>
2023-01-17 13:59:19 +1100

    fixing sizing of profile picture + centering grid for the section

--- src/App.jsx
+++ src/App.jsx
@@ -138,19 +138,18 @@ function App() {
         <ResumeDialog />
       </div>
       <section>
-        <div className='container p-6 md:mx-auto pt-[12rem]'>
-          <div className='grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12'>
-            <div className='lg:pl-20'>
-              <div className='max-w-xs px-2.5 lg:max-w-none'>
-                <img
-                  src={proflePicture}
-                  alt=''
-                  sizes='(min-width: 1024px) 32rem, 20rem'
-                  className='aspect-auto rotate-3 rounded-2xl text-zinc-800 dark:text-zinc-100 object-cover dark:bg-zinc-800'
-                />
-              </div>
+        <div className='container mx-auto flex p-6 pt-[12rem] items-center justify-center'>
+          <div className='grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:gap-y-12'>
+            <div className='flex max-w-xs px-2.5 justify-self-center lg:justify-self-end'>
+              <img
+                src={proflePicture}
+                alt=''
+                sizes='(min-width: 1024px) 32rem, 20rem'
+                className='aspect-auto rotate-3 rounded-2xl text-zinc-800 dark:text-zinc-100 object-cover dark:bg-zinc-800'
+              />
             </div>
-            <div className='lg:order-first lg:row-span-2'>
+
+            <div className='lg:order-first lg:row-span-2 justify-self-end'>
               <h1 className='text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl'>
                 Hi, I'm Moritz a Front End Developer !
               </h1>
