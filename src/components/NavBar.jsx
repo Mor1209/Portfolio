f245499678f3e9aa56be506ec0d697bb5ada857a
moritzHauptmann <mohauptmann@gmail.com>
2023-01-29 11:34:37 +1100

    fix mobile navbar text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -115,28 +115,28 @@ export default function NavBar(props) {
                       href='#'
                       className='block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700'
                     >
-                      Dashboard
+                      Me
                     </Disclosure.Button>
                     <Disclosure.Button
                       as='a'
                       href='#'
                       className='block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'
                     >
-                      Team
+                      My Skills
                     </Disclosure.Button>
                     <Disclosure.Button
                       as='a'
                       href='#'
                       className='block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'
                     >
-                      Projects
+                      My Projects
                     </Disclosure.Button>
                     <Disclosure.Button
                       as='a'
                       href='#'
                       className='block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'
                     >
-                      Calendar
+                      Get in Touch
                     </Disclosure.Button>
                   </div>
                 </Disclosure.Panel>
